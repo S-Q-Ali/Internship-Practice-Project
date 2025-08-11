f5423021c6cd21a420f84fa43c1c3ab1a36dc974
S-Q-Ali <syedqasim963gmail.com>
2025-08-11 04:12:39 -0700

    Shop by Category Section Added

---
+++ my-project/src/components/CategorySection.jsx
@@ -0,0 +1,11 @@
+export default function CategorySection(){
+    return (
+        <>
+        <section className="relative w-full h-3/12 from-zinc-900 to-purple-600">  
+        <div className="w-full">
+            <h1 className="text-3xl font-semibold py-8 text-center">SHOP BY CATEGORY</h1>
+        </div>
+        </section>
+        </>
+    );
+}
