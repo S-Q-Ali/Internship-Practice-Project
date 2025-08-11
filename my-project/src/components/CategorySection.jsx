62d722f5b2eb1aa43cbc1dc59ded088ee9cd0d0f
S-Q-Ali <syedqasim963gmail.com>
2025-08-11 04:53:49 -0700

    Images routes/paths added

--- my-project/src/components/CategorySection.jsx
+++ my-project/src/components/CategorySection.jsx
@@ -1,13 +1,18 @@
 import headphone from "../../assets/headphones/headphone_1.png";
-import keyboard from "../../assets/keyborads/keyboard_1.png";
-import mouse from "../../assets/mouses/mouse_1.png";
-import speaker from "../../assets/speakers/speaker_1.png";
+import keyboard from "../../assets/keyborads/keyboard_3.png";
+import mouse from "../../assets/mouses/mouse_3.png";
+import speaker from "../../assets/speakers/speaker_3.png";
+import { useNavigate } from "react-router-dom";
 
 export default function CategorySection() {
+    const navigate=useNavigate();
+    const handleCategoryClick=(category)=>{
+        navigate("/categories",{state:{scrollTo:category}});
+    };
   return (
     <section className="flex w-full bg-gradient-to-b from-black to-zinc-950 py-16">
       <div className="w-full flex flex-col items-center text-center">
-        <h1 className="text-amber-50 text-3xl font-semibold m-8">
+        <h1 className="text-amber-50 text-4xl font-semibold mb-14">
           SHOP BY CATEGORY
         </h1>
 
@@ -16,21 +21,25 @@ export default function CategorySection() {
             src={headphone}
             alt="Headphone Category"
             className="w-44 h-44 object-contain cursor-pointer hover:scale-110 transition-transform duration-300 border-2 border-purple-500 rounded-full"
+            onClick={()=>handleCategoryClick("headphones")}
           />
           <img
             src={keyboard}
             alt="Keyboard Category"
             className="w-44 h-44 object-contain cursor-pointer hover:scale-110 transition-transform duration-300 border-2 border-purple-500 rounded-full"
+            onClick={()=>handleCategoryClick("keyboard")}
           />
           <img
             src={mouse}
             alt="Mouse Category"
             className="w-44 h-44 object-contain cursor-pointer hover:scale-110 transition-transform duration-300 border-2 border-purple-500 rounded-full"
+          onClick={()=>handleCategoryClick("mouse")}
           />
           <img
             src={speaker}
             alt="Speaker Category"
             className="w-44 h-44 object-contain cursor-pointer hover:scale-110 transition-transform duration-300 border-2 border-purple-500 rounded-full"
+          onClick={()=>{handleCategoryClick("speaker")}}
           />
         </div>
       </div>
